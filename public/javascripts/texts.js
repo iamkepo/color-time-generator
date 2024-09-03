57184a90a39bae3b599282c1ab4183800eb4f44c
Christ-Amour K <christ-amour@wazoom-studio.com>
2024-09-03 21:11:07 +0100

    feat : public view

---
+++ public/javascripts/texts.js
@@ -0,0 +1,16 @@
+export const bodyTexts = {
+  en: {
+    TITLE: 'Color Time',
+    DESCRIPTION: 'Welcome to Color Time Generator',
+    START: 'Get starting',
+    LABEL: 'Date update',
+    SEARCH: 'Search'
+  },
+  fr: {
+    TITLE: 'Temps Couleur',
+    DESCRIPTION: 'Bienvenu au Génerateur de Couleur par Temps',
+    START: 'Commencer',
+    LABEL: 'Mise à jour de la date',
+    SEARCH: 'Recherche'
+  }
+};
